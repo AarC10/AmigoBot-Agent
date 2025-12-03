5442a3ff396753c3a1eb3eaf2c2a181363b522de
Aaron Chan <aarchan108@gmail.com>
2025-12-03 16:27:01 -0500

    Fix my zsh theme stuff from being copied as poart of gstreamer script

--- src/picam_forwarder/gstreamer_udp.sh
+++ src/picam_forwarder/gstreamer_udp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gst-launch-1.0 \                                                                                      ✔  12s   ▓▒░
+gst-launch-1.0 \
   v4l2src device=/dev/video0 ! \
   video/x-raw,width=640,height=480,framerate=10/1 ! \
   videoconvert ! \
